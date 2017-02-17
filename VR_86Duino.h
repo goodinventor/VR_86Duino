9b78b08234d947f173ebabe415c484c1fa108ae9
goodinventor <gandrews20143@gmail.com>
2017-02-17 06:31:15 -0500

    Added class Camera constructors to VR_86Duino.h

--- VR_86Duino.h
+++ VR_86Duino.h
@@ -84,6 +84,8 @@ class Camera
   int ObjectsInWorldSize;
 
   // functions
+  Camera();
+  Camera(int min_x, int max_x, int min_y, int max_y, int min_z, int max_z);
   void DrawScene(UTFT screen);
 };
 
